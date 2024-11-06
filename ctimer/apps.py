8af0a57456205b8f0782c470ac189c2f831e47c0
JohnsonMourya <johnsonmouryan17@gmail.com>
2024-11-06 19:52:15 +0530

    first and last commit form windows

---
+++ ctimer/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class CtimerConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'ctimer'
---
+++ unitconvertor/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class UnitconvertorConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'unitconvertor'
